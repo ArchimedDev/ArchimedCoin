97fdb8c8ba724eb96336bebe57625a278d12cd6e
ArhimedCoin <ArhimedCoin>
2018-03-09 02:25:00 +0300

    Update port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 30211;
+        nRPCPort = 3121;
     }
 };
 static CBaseMainParams mainParams;
