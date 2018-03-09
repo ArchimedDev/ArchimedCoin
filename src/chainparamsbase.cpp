92f8220cc704787157bd77bbe1a09023fa309762
ArhimedCoin <ArhimedCoin>
2018-03-09 03:28:04 +0300

    update

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 10211;
+        nRPCPort = 30211;
     }
 };
 static CBaseMainParams mainParams;
